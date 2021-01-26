5779c45f0da77b08c0bc3dc2a3a0d1a804795c0a
Nathan Herrmann <nherrmann@gmail.com>
2021-01-26 14:00:44 -0500

    Exercise 1.8

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,11 +6,17 @@ locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
 principal = 500000.0
 rate = 0.05
-payment = 2684.11
+base_payment = 2684.11
+extra_payment = base_payment + 1000
 total_paid = 0.0
+month = 1
 
 while principal > 0:
-	principal = principal * (1+rate/12) - payment
-	total_paid = total_paid + payment
+    if month <= 12:
+        principal = principal * (1+rate/12) - extra_payment
+        total_paid = total_paid + extra_payment
+    elif month > 12:
+        principal = principal * (1+rate/12) - base_payment
+        total_paid = total_paid + base_payment
 
 print('Total paid:', locale.currency(total_paid, grouping=True))
